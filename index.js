91235b4493478b75bcdc4a516c0a96de4df556e6
Andrew Davisson <akdavisson4@gmail.com>
2020-05-12 08:50:27 -0600

    learn input

--- index.js
+++ index.js
@@ -40,5 +40,11 @@ let app5 = new Vue({
       this.message = this.message.split('').reverse().join('')
     }
   }
+})
 
+let app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Hello Vue.js',
+  }
 })
