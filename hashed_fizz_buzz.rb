7abde06e40b6a49e4fcdc9653d4854db0e35cca7
Andrew Davisson <akdavisson4@gmail.com>
2020-07-07 08:38:00 -0600

    created rspec test

--- hashed_fizz_buzz.rb
+++ hashed_fizz_buzz.rb
@@ -1,3 +1,5 @@
+require 'rspec'
+
 def hashed_fizz_buzz(limit)
   fizz_buzz_obj = Hash.new
 
@@ -13,10 +15,22 @@ def hashed_fizz_buzz(limit)
     end
   end
 
-  puts fizz_buzz_obj
+  fizz_buzz_obj
 end
 
-puts "Enter a number: "
-limit = gets.strip
+def user_interaction
+  puts "Enter a number: "
+  limit = gets.strip
+  puts hashed_fizz_buzz(limit.to_i)
+end
 
-hashed_fizz_buzz(limit.to_i)
+user_interaction
+
+#Tests
+
+describe "Hashed Fizz Buzz" do
+ it "produces the correct has for '25'" do
+  completed_hash = {1=>1, 2=>2, 3=>"Fizz", 4=>4, 5=>"Buzz", 6=>"Fizz", 7=>7, 8=>8, 9=>"Fizz", 10=>"Buzz", 11=>11, 12=>"Fizz", 13=>13, 14=>14, 15=>"FizzBuzz", 16=>16, 17=>17, 18=>"Fizz", 19=>19, 20=>"Buzz", 21=>"Fizz", 22=>22, 23=>23, 24=>"Fizz", 25=>"Buzz"}
+  expect(hashed_fizz_buzz(25)).to eq(completed_hash)
+ end
+end
