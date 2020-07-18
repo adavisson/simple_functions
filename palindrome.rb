4343fe3a76b8bd91c4de0f05d3939ffcb6358249
Andrew Davisson <akdavisson4@gmail.com>
2020-07-18 07:29:40 -0600

    added test

--- palindrome.rb
+++ palindrome.rb
@@ -1,11 +1,12 @@
+require 'rspec'
+
 # Palindrome takes in an array, compares the first and last letter
 # and then recursively calls itself
 def palindrome(word)
 
   # If you reach the middle then the word is a palindrome
   if(word.length <=1)
-    puts "It is a palindrome"
-    return
+    return "It is a palindrome"
   end
 
   # Get the first and last letter of array
@@ -18,8 +19,7 @@ def palindrome(word)
   if (first.downcase == last.downcase)
     palindrome(rest)
   else
-    puts "It is not a palindrome"
-    return
+    return "It is not a palindrome"
   end
 end
 
@@ -28,6 +28,26 @@ puts "Enter a word: "
 word = gets.strip
 
 # Call palindrome function with user input
-palindrome(word.split('').select do |el| 
+puts palindrome(word.split('').select do |el| 
   el != ' '
 end)
+
+describe 'Palindrome' do
+  it 'recognizes when a word is a palindorome' do
+    expect(palindrome("racecar".split('').select do |el|
+      el != ' '
+    end)).to eq("It is a palindrome")
+    expect(palindrome("RAcE caR".split('').select do |el|
+      el != ' '
+    end)).to eq("It is a palindrome")
+  end
+
+  it 'recognizes when a word is not a palindrome' do
+    expect(palindrome("dog".split('').select do |el|
+      el != ' '
+    end)).to eq("It is not a palindrome")
+    expect(palindrome("horse".split('').select do |el|
+      el != ' '
+    end)).to eq("It is not a palindrome")
+  end
+end
